512cd34c5e4fa694ae3ab375a40b8b81264dbb27
Glitch (bolder-spotless-steed) <none>
2020-03-08 15:34:16 +0000

    🐽🐥 Checkpoint ./style.css:12615382/56 ./index.html:12615382/134 ./script.js:12615382/133

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function showMenu() {
   document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("pageContent").style.marginLeft = "250px";
+  document.getElementById("btnOpen").style.display="none";
 }
 
 function hideMenu() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("pageContent").style.marginLeft= "0";
+  document.getElementById("btnOpen").style.display="block";
 }
