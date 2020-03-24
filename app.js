a9c6b50d37c87586e68dea4e3a3772d2c419280e
shadealao <alao.a.s@gmail.com>
2020-03-24 12:00:48 +0100

    changement des pages .html en .ejs pour ne pas à réécrire le menu, le head et le footer dans toutes les pages

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -11,6 +12,7 @@ var inscriptionRouter = require('./routes/inscription');
 var connexionRouter = require('./routes/connexion');
 var profileRouter = require('./routes/profil');
 var proposerRouter = require('./routes/proposer');
+var messagerieRouter = require('./routes/messagerie');
 
 var app = express();
 
@@ -21,13 +23,14 @@ var cons = require('consolidate');
 // view engine setup
 app.engine('html', cons.swig)
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -36,21 +39,62 @@ app.use('/inscription', inscriptionRouter);
 app.use('/connexion', connexionRouter);
 app.use('/rechercher', rechercherRouter);
 app.use('/proposer',proposerRouter);
+app.use('/messagerie',messagerieRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
+
+
+
+//messagerie
+/*let io = require('socket.io');
+let lastMessage = new Array();
+io.on('connection', function(client){
+
+client.emit('last message', lastMessage);
+
+
+client.on('new message', function(data){
+    // Vérification du pseudonyme
+    if(!data.username || typeof data.username == undefined || data.username.length > 25){
+        client.emit('error message', "Le pseudonyme rentré n'est pas valide !");
+        return;
+    }
+
+    // Vérification du message
+    if(!data.message || typeof data.message == undefined || data.message.length > 255){
+        client.emit('error message', "Le message rentré n'est pas valide !");
+        return;
+    }
+    lastMessage.push(data.username + ' : ' + data.message);
+    for(var i = lastMessage.length-1; i--;){
+        if(i == 4){
+            lastMessage.shift();
+        }
+    }
+    io.emit('new message', data);
+});
+
+client.on('disconnect', function(){
+    delete client;
+});
+
+});
+*/
+
+
 
 module.exports = app;
